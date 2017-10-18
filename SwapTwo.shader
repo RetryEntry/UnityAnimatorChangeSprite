c73f2f4bd7b36a5161e80a0fc329c95621bd65f5
RetryEntry <retry.entry@gmail.com>
2017-10-18 13:25:14 +0300

    Add files via upload

---
+++ SwapTwo.shader
@@ -0,0 +1,62 @@
+﻿Shader "Custom/SwapTwo" {
+     Properties
+     {
+         [PerRendererData] _MainTex ("Sprite Texture", 2D) = "white" {}
+         _MainTex2 ("_MainTex2", 2D) = "white" {} 
+         _Color ("Tint", Color) = (1,1,1,1)
+         [MaterialToggle] PixelSnap ("Pixel snap", Float) = 0
+     }
+ 
+     SubShader
+     {
+         Tags
+         { 
+             "Queue"="Transparent" 
+             "IgnoreProjector"="True" 
+             "RenderType"="Transparent" 
+             "PreviewType"="Plane"
+             "CanUseSpriteAtlas"="True"
+         }
+ 
+         Cull Off
+         Lighting Off
+         ZWrite Off
+         Blend One OneMinusSrcAlpha
+ 
+         CGPROGRAM
+         #pragma surface surf Lambert vertex:vert nofog keepalpha
+         #pragma multi_compile _ PIXELSNAP_ON
+         #pragma multi_compile _ ETC1_EXTERNAL_ALPHA
+ 
+         sampler2D _MainTex;
+         fixed4 _Color;
+		 sampler2D _MainTex2;
+
+         struct Input
+         {
+             float2 uv_MainTex;
+             fixed4 color;
+         };
+         
+         void vert (inout appdata_full v, out Input o)
+         {
+             #if defined(PIXELSNAP_ON)
+             v.vertex = UnityPixelSnap (v.vertex);
+             #endif
+             
+             UNITY_INITIALIZE_OUTPUT(Input, o);
+             o.color = v.color * _Color;
+         }
+  
+         void surf (Input IN, inout SurfaceOutput o)
+         {		
+			half4 c = tex2D (_MainTex2, IN.uv_MainTex) * IN.color;
+
+			 o.Albedo = c.rgb * c.a;
+             o.Alpha = c.a;
+         }
+         ENDCG
+     }
+ 
+ Fallback "Transparent/VertexLit"
+ }
---
+++ SwappedTextures.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwappedTextures : MonoBehaviour {
+    public Texture2D swappedTexture = null;
+
+    private SpriteRenderer thisRenderer;
+    private void Awake()
+    {
+        thisRenderer = GetComponent<SpriteRenderer>();
+        if (thisRenderer)
+        {
+            Shader _swapShader = Shader.Find("Custom/SwapTwo");
+            if (!_swapShader)
+            {
+                Debug.LogError("You dont have shader... ");
+            }
+            else
+            {
+                Material _newMat = new Material(_swapShader);
+                thisRenderer.material = _newMat;
+                thisRenderer.material.SetTexture("_MainTex2", swappedTexture);
+            }
+        }
+        else
+        {
+            Debug.LogError("There is NO spriterenderer attached to gameobject " + this.name);
+        }
+    }
+
+    /// <summary>
+    /// this will swap our used animator texture to another one
+    /// </summary>
+    /// <param name="_toWhat"></param>
+    public void SwapTexture(Texture2D _toWhat)
+    {
+        swappedTexture = _toWhat;
+        if (thisRenderer)
+        {
+            Shader _swapShader = Shader.Find("Custom/SwapTwo");
+            if (!_swapShader)
+            {
+                Debug.LogError("You dont have shader... ");
+            }
+            else
+            {
+                Material _newMat = new Material(_swapShader);
+                thisRenderer.material = _newMat;
+                thisRenderer.material.SetTexture("_MainTex2", swappedTexture);
+            }
+        }
+        else
+        {
+            Debug.LogError("There is NO spriterenderer attached to gameobject " + this.name);
+        }
+    }
+
+    public void Update()
+    {
+        if (swappedTexture != null)
+        {
+            SwapTexture(swappedTexture);
+            swappedTexture = null;
+        }
+    }
+}
